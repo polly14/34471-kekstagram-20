47e5d434700c40b3bfdcbb43974d351a46b35081
Polina Bakova <bakovapolina@mail.ru>
2020-06-19 18:27:59 +0300

    fix

---
+++ js/preview.js
@@ -0,0 +1,39 @@
+'use strict';
+(function () {
+  var uploadFile = document.querySelector('#upload-file');
+  var imgUploadOverlay = document.querySelector('.img-upload__overlay');
+  var uploadCancel = document.querySelector('#upload-cancel');
+  var textHashtags = document.querySelector('.text__hashtags');
+  var textDescription = document.querySelector('.text__description');
+  var onPopupEscPress = function (evt) {
+    window.utils.isEscEvent(evt, closePopup);
+  };
+  var openPopup = function () {
+    imgUploadOverlay.classList.remove('hidden');
+    document.querySelector('body').classList.add('modal-open');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+  var closePopup = function () {
+    if (textHashtags !== document.activeElement && textDescription !== document.activeElement) {
+      imgUploadOverlay.classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+      document.removeEventListener('keydown', onPopupEscPress);
+      uploadFile.value = '';
+    }
+  };
+  uploadFile.addEventListener('change', function () {
+    openPopup();
+  });
+  uploadFile.addEventListener('keydown', function (evt) {
+    window.utils.isEnterEvent(evt, openPopup);
+  });
+  uploadCancel.addEventListener('click', function () {
+    closePopup();
+  });
+  uploadCancel.addEventListener('keydown', function (evt) {
+    window.utils.isEnterEvent(evt, closePopup);
+  });
+
+})();
+
+
